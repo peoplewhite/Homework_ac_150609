fd01297460690ec371d150ddcc9b18816243b998
peoplewhite <shonshon7@gmail.com>
2015-06-09 19:46:47 +0800

    first commit

---
+++ Homework_150609/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  Homework_150609
+//
+//  Created by Po-Hao Cheng on 2015/6/9.
+//  Copyright (c) 2015年 phc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction) button1 {
+    NSLog(@"首頁");
+}
+- (IBAction) button2 {
+    NSLog(@"頻道");
+}
+- (IBAction) button3 {
+    NSLog(@"我的千尋");
+}
+- (IBAction) button4 {
+    NSLog(@"廣場");
+}
+- (IBAction) button5 {
+    NSLog(@"更多");
+}
+@end
